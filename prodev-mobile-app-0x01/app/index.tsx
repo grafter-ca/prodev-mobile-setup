23daad9aa98e175f91eb4ebab45b498686e28217
habyarimana caleb <callebhabyar55@gmail.com>
2025-08-27 14:25:17 +0200

    updare index.tsx in app directory

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -13,24 +13,6 @@ export default function Index() {
         </Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
-      <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
-      <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
     </View>
   );
 }
