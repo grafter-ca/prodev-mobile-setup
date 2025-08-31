831a411c86b9310f9a866325e67dded1f8390f20
habyarimana caleb <callebhabyar55@gmail.com>
2025-08-31 23:47:59 +0200

    update index

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
       
       <View>
         <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native_provides YOU a single codebase for_cross.platforms</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
